3c2d3b6522618c6c447ccb14cf2ae11602333b9b
崔佳佳 <56425637+Sir0813@users.noreply.github.com>
2020-09-15 13:10:25 +0800

    接口调整

--- src/main/java/com/cmdc/ShiroApplication.java
+++ src/main/java/com/cmdc/ShiroApplication.java
@@ -3,7 +3,12 @@ package com.cmdc;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.jdbc.datasource.DataSourceTransactionManager;
+import org.springframework.transaction.PlatformTransactionManager;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+import javax.sql.DataSource;
 
 /**
  * 流程:
@@ -22,4 +27,12 @@ public class ShiroApplication {
     public static void main(String[] args) {
         SpringApplication.run(ShiroApplication.class,args);
     }
+
+    // 添加数据源
+    @Bean
+    public PlatformTransactionManager txManager(DataSource dataSource) {
+        return new DataSourceTransactionManager(dataSource);
+    }
+
+
 }
